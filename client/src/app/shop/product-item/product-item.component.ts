fbab6dee97c581cb03aaa1bb7222dd3924b4b128
Marko Dankovich <markodankovic@protonmail.com>
2023-10-18 14:43:21 +0200

    fixed imgs not loading

--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { BasketService } from 'src/app/basket/basket.service';
 import { Product } from 'src/app/shared/models/product';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss']
 })
 export class ProductItemComponent {
+  env = environment;
   @Input() product?: Product;
 
   constructor(private basketService: BasketService ) {}
